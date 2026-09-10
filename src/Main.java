b6da676176f9eecc8453a1a6e4dc81a48dc9d678
Gabriel Hagmayer <gabrielhagmayer@gmail.com>
2026-09-09 21:14:36 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import org.w3c.dom.ls.LSOutput;
+
+import javax.swing.*;
+
+public class Main {
+    static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "hello word");
+    }
+}
